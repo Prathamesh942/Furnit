cd4af80e9823b4de48897a6e660a72b0d1faea5f
prathamesh <prathameshsk94@gmail.com>
2024-02-23 15:27:49 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
@@ -6,16 +7,44 @@ import Product from "./pages/Product";
 import Shop from "./pages/Shop";
 import Wishlist from "./pages/Wishlist";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/shop",
+    element: <Shop />,
+  },
+  {
+    path: "/product",
+    element: <Product />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "/wishlist",
+    element: <Wishlist />,
+  },
+]);
+
 function App() {
   return (
-    <div>
-      {/* <Home /> */}
-      {/* <Shop /> */}
-      {/* <Product /> */}
-      {/* <Cart /> */}
-      {/* <Checkout /> */}
-      <Wishlist />
-    </div>
+    <RouterProvider router={router} />
+    // <div>
+    //   {/* <Home /> */}
+    //   {/* <Shop /> */}
+    //   {/* <Product /> */}
+    //   {/* <Cart /> */}
+    //   {/* <Checkout /> */}
+    //   <Wishlist />
+    // </div>
   );
 }
 
